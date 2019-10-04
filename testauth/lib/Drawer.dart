164c517924703e8e52edd2b2813c6d20d1e95e0e
jokerhmed <jokerhmed11197@gmail.com>
2019-10-04 14:26:12 +0200

    Drawer added

---
+++ testauth/lib/Drawer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:testauth/ProfileScreen.dart';
+
+
+class Drawerperso extends StatelessWidget {
+  final UserDetails detailsUser;
+
+  Drawerperso({Key key, @required this.detailsUser}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final DrawerHeader = UserAccountsDrawerHeader(
+      accountName: Text(detailsUser.userName,style: TextStyle(fontSize: 22.0),),
+      accountEmail: Text(detailsUser.userEmail),
+      currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage(detailsUser.photoUrl),
+
+        backgroundColor: Colors.white,
+      ),
+    );
+    final Draweritems = ListView(
+      children: <Widget>[
+        DrawerHeader,
+        ListTile(onTap: (){},
+          title: Text("wishlist",style: TextStyle(fontSize: 16.0),),
+        ),
+        ListTile(
+          title: Text("wishlist",style: TextStyle(fontSize: 16.0),),
+        ),
+        ListTile(
+          title: Text("wishlist",style: TextStyle(fontSize: 16.0),),
+        )
+      ],
+    );
+    return Drawer(child: Draweritems,);
+  }
+
+}
